f34191028237caba96f3eb6a707b14ad2c1c4a7e
Antonio Lain <antlai@cafjs.com>
2018-07-28 20:15:10 -0700

    Fix local port change for device

--- simdevice.js
+++ simdevice.js
@@ -45,20 +45,26 @@ var isXip = function(x) {
     return x.split(':')[0].endsWith(XIP_SUFFIX);
 };
 
+var isLocal = function(x) {
+    return x.split(':')[0].endsWith(DEFAULT_APP_SUFFIX);
+};
+
 if (typeof spec.env.ipAddress === 'string') {
     spec.env.appSuffix =  spec.env.ipAddress +  '.' + XIP_SUFFIX +
         (spec.env.port ?  ':' + spec.env.port : '');
+} else if (spec.env.port) {
+    spec.env.appSuffix = (spec.env.appSuffix ? spec.env.appSuffix :
+                          DEFAULT_APP_SUFFIX) + ':' + spec.env.port;
 }
 
-if ((spec.env.appSuffix !== DEFAULT_APP_SUFFIX) && !isXip(spec.env.appSuffix)) {
+if (!isLocal(spec.env.appSuffix) && !isXip(spec.env.appSuffix)) {
     spec.env.appProtocol = 'https';
     spec.env.accountsURL = 'https://root-accounts.' + spec.env.appSuffix;
-};
-
-if (isXip(spec.env.appSuffix)) {
+} else {
     spec.env.appProtocol = 'http';
     spec.env.accountsURL = 'http://root-accounts.' + spec.env.appSuffix;
-}
+};
+
 
 spec.env.configVolume = spec.env.rootDir + '/' + spec.env.deviceId +
     '/config';
